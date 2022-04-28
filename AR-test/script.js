682797c5d09202c874a7c02a3c531f9620cb0c2c
masha-krotkykh <31903446+masha-krotkykh@users.noreply.github.com>
2022-04-28 14:45:01 -0400

    Add files via upload

---
+++ AR-test/script.js
@@ -0,0 +1,35 @@
+const modelViewer = document.querySelector( 'model-viewer' );
+let animationOpen = false;
+
+// Handles loading the events for <model-viewer>'s slotted progress bar
+const onProgress = (event) => {
+  const progressBar = event.target.querySelector('.progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add('hide');
+  } else {
+    progressBar.classList.remove('hide');
+    // if (event.detail.totalProgress === 0) {
+    //   event.target.querySelector('.center-pre-prompt').classList.add('hide');
+    // }
+  }
+};
+modelViewer.addEventListener('progress', onProgress);
+
+function playAnimation() {
+  if(!animationOpen) {
+    modelViewer.timeScale = 0.7;
+    modelViewer.play({repetitions: 1});
+    animationOpen = true;
+  }
+
+  else {
+    modelViewer.timeScale = -0.7;
+    modelViewer.play({repetitions: 1});
+    animationOpen = false;
+  }
+
+  console.log(animationOpen);
+
+}
